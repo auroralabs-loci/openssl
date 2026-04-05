auroralabs-loci/openssl

commit 94e6ae7a697486716313062c5c6700c9e0d6423a

Submitted by: "Victor B. Wagner" <vitus@cryptocom.ru>
Make {d2i,i2d}_PrivateKey() fall back to PKCS#8 format if no legacy format
supported. Add support in d2i_AutoPrivateKey().

crypto/asn1/d2i_pr.c
@@ -64,6 +64,7 @@
 #ifndef OPENSSL_NO_ENGINE
 #include <openssl/engine.h>
 #endif
+#include <openssl/x509.h>
 #include <openssl/asn1.h>
 #include "asn1_locl.h"
 
@@ -101,9 +102,22 @@ EVP_PKEY *d2i_PrivateKey(int type, EVP_PKEY **a, const unsigned char **pp,
 	if (!ret->ameth->old_priv_decode ||
 			!ret->ameth->old_priv_decode(ret, pp, length))
 		{
-		ASN1err(ASN1_F_D2I_PRIVATEKEY,ERR_R_ASN1_LIB);
-		goto err;
-		}
+		if (ret->ameth->priv_decode) 
+			{
+			PKCS8_PRIV_KEY_INFO *p8=NULL;
+			p8=d2i_PKCS8_PRIV_KEY_INFO(NULL,pp,length);
+			if (!p8) goto err;
+			EVP_PKEY_free(ret);
+			ret = EVP_PKCS82PKEY(p8);
+			PKCS8_PRIV_KEY_INFO_free(p8);
+
+			} 
+		else 
+			{
+			ASN1err(ASN1_F_D2I_PRIVATEKEY,ERR_R_ASN1_LIB);
+			goto err;
+			}
+		}	
 	if (a != NULL) (*a)=ret;
 	return(ret);
 err:
@@ -132,6 +146,24 @@ EVP_PKEY *d2i_AutoPrivateKey(EVP_PKEY **a, const unsigned char **pp,
 		keytype = EVP_PKEY_DSA;
 	else if (sk_ASN1_TYPE_num(inkey) == 4)
 		keytype = EVP_PKEY_EC;
+	else if (sk_ASN1_TYPE_num(inkey) == 3)  
+		{ /* This seems to be PKCS8, not traditional format */
+			PKCS8_PRIV_KEY_INFO *p8 = d2i_PKCS8_PRIV_KEY_INFO(NULL,pp,length);
+			EVP_PKEY *ret;
+
+			sk_ASN1_TYPE_pop_free(inkey, ASN1_TYPE_free);
+			if (!p8) 
+				{
+				ASN1err(ASN1_F_D2I_AUTOPRIVATEKEY,ASN1_R_UNSUPPORTED_PUBLIC_KEY_TYPE);
+				return NULL;
+				}
+			ret = EVP_PKCS82PKEY(p8);
+			PKCS8_PRIV_KEY_INFO_free(p8);
+			if (a) {
+				*a = ret;
+			}	
+			return ret;
+		}
 	else keytype = EVP_PKEY_RSA;
 	sk_ASN1_TYPE_pop_free(inkey, ASN1_TYPE_free);
 	return d2i_PrivateKey(keytype, a, pp, length);

crypto/asn1/asn1.h
@@ -1185,6 +1185,7 @@ void ERR_load_ASN1_strings(void);
 #define ASN1_F_D2I_ASN1_TYPE_BYTES			 149
 #define ASN1_F_D2I_ASN1_UINTEGER			 150
 #define ASN1_F_D2I_ASN1_UTCTIME				 151
+#define ASN1_F_D2I_AUTOPRIVATEKEY			 207
 #define ASN1_F_D2I_NETSCAPE_RSA				 152
 #define ASN1_F_D2I_NETSCAPE_RSA_2			 153
 #define ASN1_F_D2I_PRIVATEKEY				 154

crypto/asn1/asn1_err.c
@@ -1,6 +1,6 @@
 /* crypto/asn1/asn1_err.c */
 /* ====================================================================
- * Copyright (c) 1999-2006 The OpenSSL Project.  All rights reserved.
+ * Copyright (c) 1999-2007 The OpenSSL Project.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -147,6 +147,7 @@ static ERR_STRING_DATA ASN1_str_functs[]=
 {ERR_FUNC(ASN1_F_D2I_ASN1_TYPE_BYTES),	"d2i_ASN1_type_bytes"},
 {ERR_FUNC(ASN1_F_D2I_ASN1_UINTEGER),	"d2i_ASN1_UINTEGER"},
 {ERR_FUNC(ASN1_F_D2I_ASN1_UTCTIME),	"D2I_ASN1_UTCTIME"},
+{ERR_FUNC(ASN1_F_D2I_AUTOPRIVATEKEY),	"d2i_AutoPrivateKey"},
 {ERR_FUNC(ASN1_F_D2I_NETSCAPE_RSA),	"d2i_Netscape_RSA"},
 {ERR_FUNC(ASN1_F_D2I_NETSCAPE_RSA_2),	"D2I_NETSCAPE_RSA_2"},
 {ERR_FUNC(ASN1_F_D2I_PRIVATEKEY),	"d2i_PrivateKey"},

crypto/asn1/i2d_pr.c
@@ -59,6 +59,7 @@
 #include <stdio.h>
 #include "cryptlib.h"
 #include <openssl/evp.h>
+#include <openssl/x509.h>
 #include "asn1_locl.h"
 
 int i2d_PrivateKey(EVP_PKEY *a, unsigned char **pp)
@@ -67,6 +68,12 @@ int i2d_PrivateKey(EVP_PKEY *a, unsigned char **pp)
 		{
 		return a->ameth->old_priv_encode(a, pp);
 		}
+	if (a->ameth && a->ameth->priv_encode) {
+		PKCS8_PRIV_KEY_INFO *p8 = EVP_PKEY2PKCS8(a);
+		int ret = i2d_PKCS8_PRIV_KEY_INFO(p8,pp);
+		PKCS8_PRIV_KEY_INFO_free(p8);
+		return ret;
+	}	
 	ASN1err(ASN1_F_I2D_PRIVATEKEY,ASN1_R_UNSUPPORTED_PUBLIC_KEY_TYPE);
 	return(-1);
 	}

commit 0a62ff723e5f481d5d35dc64a8e5e2e60b659669

Fix dangling pointer in ossl_d2i_PrivateKey_legacy()
When called with *a != NULL and the PKCS#8 re-parse path succeeds but
the decoded key type does not match the requested keytype, the function
frees *a via EVP_PKEY_free(ret) but never sets *a to NULL.  The caller
is left with a dangling pointer and will likely double-free it.

Set *a to NULL immediately after freeing it on the PKCS#8 path.

Fixes: 94e6ae7a69 ("Make {d2i,i2d}_PrivateKey() fall back to PKCS#8 format")
Fixes: 444b25b1e9 ("Add back in legacy paths for d2i_PrivateKey/d2i_AutoPrivateKey.")

crypto/asn1/d2i_pr.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 1995-2025 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 1995-2026 The OpenSSL Project Authors. All Rights Reserved.
  *
  * Licensed under the Apache License 2.0 (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
@@ -133,6 +133,8 @@ ossl_d2i_PrivateKey_legacy(int keytype, EVP_PKEY **a, const unsigned char **pp,
                 goto err;
             }
             EVP_PKEY_free(ret);
+            if (a != NULL)
+                *a = NULL;
             ret = tmp;
             ERR_pop_to_mark();
             if (EVP_PKEY_type(keytype) != EVP_PKEY_get_base_id(ret))

test/evp_extra_test2.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2025 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 2015-2026 The OpenSSL Project Authors. All Rights Reserved.
  *
  * Licensed under the Apache License 2.0 (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
@@ -3544,6 +3544,98 @@ static int evp_test_name_parsing(void)
     return 1;
 }
 
+/*
+ * Test that d2i_PrivateKey() NULLs out *a when the legacy path frees it
+ * internally due to a key type mismatch. Before the fix, *a was left
+ * as a dangling pointer.
+ */
+static int test_d2i_PrivateKey_type_mismatch(void)
+{
+    int ok = 0;
+    OSSL_PROVIDER *provider = NULL;
+    EVP_PKEY *rsa_key = NULL;
+    EVP_PKEY *saved_ptr = NULL;
+    EVP_PKEY *result = NULL;
+    EVP_PKEY *ec_key = NULL;
+    EVP_PKEY_CTX *gctx = NULL;
+    PKCS8_PRIV_KEY_INFO *p8inf = NULL;
+    unsigned char *ec_der = NULL, *ec_der_tmp = NULL;
+    const unsigned char *p;
+    int ec_der_len;
+
+    if (!TEST_ptr(provider = OSSL_PROVIDER_load(NULL, "default")))
+        goto err;
+
+    /* Generate an EC P-256 key */
+    gctx = EVP_PKEY_CTX_new_from_name(NULL, "EC", NULL);
+    if (!TEST_ptr(gctx)
+        || !TEST_int_gt(EVP_PKEY_keygen_init(gctx), 0)
+        || !TEST_int_gt(EVP_PKEY_CTX_set_group_name(gctx, "P-256"), 0)
+        || !TEST_int_gt(EVP_PKEY_keygen(gctx, &ec_key), 0))
+        goto err;
+    EVP_PKEY_CTX_free(gctx);
+    gctx = NULL;
+
+    /* Encode as PKCS#8 DER so the legacy PKCS#8 path is exercised */
+    p8inf = EVP_PKEY2PKCS8(ec_key);
+    if (!TEST_ptr(p8inf))
+        goto err;
+    ec_der_len = i2d_PKCS8_PRIV_KEY_INFO(p8inf, NULL);
+    if (!TEST_int_gt(ec_der_len, 0))
+        goto err;
+    ec_der = OPENSSL_malloc(ec_der_len);
+    if (!TEST_ptr(ec_der))
+        goto err;
+    ec_der_tmp = ec_der;
+    if (!TEST_int_eq(i2d_PKCS8_PRIV_KEY_INFO(p8inf, &ec_der_tmp), ec_der_len))
+        goto err;
+
+    /* Generate an RSA key as the existing *a */
+    gctx = EVP_PKEY_CTX_new_from_name(NULL, "RSA", NULL);
+    if (!TEST_ptr(gctx)
+        || !TEST_int_gt(EVP_PKEY_keygen_init(gctx), 0)
+        || !TEST_int_gt(EVP_PKEY_CTX_set_rsa_keygen_bits(gctx, 2048), 0)
+        || !TEST_int_gt(EVP_PKEY_keygen(gctx, &rsa_key), 0))
+        goto err;
+
+    saved_ptr = rsa_key;
+
+    /* Extra ref so the free inside the function does not deallocate */
+    if (!TEST_true(EVP_PKEY_up_ref(rsa_key)))
+        goto err;
+
+    /* RSA requested but EC PKCS#8 given -- triggers type mismatch path */
+    p = ec_der;
+    ERR_clear_error();
+    result = d2i_PrivateKey(EVP_PKEY_RSA, &rsa_key, &p, ec_der_len);
+
+    if (!TEST_ptr_null(result)) {
+        /* Modern decoder handled it; legacy path not taken */
+        EVP_PKEY_free(result);
+        EVP_PKEY_free(saved_ptr);
+        ok = 1;
+        goto err;
+    }
+
+    /*
+     * After the fix, *a must be NULL because the function freed the
+     * original key internally.  Before the fix, *a would still point
+     * to freed memory (dangling pointer).
+     */
+    if (!TEST_ptr_null(rsa_key))
+        TEST_info("*a not NULLed after internal free -- dangling pointer bug");
+    else
+        ok = 1;
+
+    EVP_PKEY_free(saved_ptr);
+err:
+    EVP_PKEY_CTX_free(gctx);
+    EVP_PKEY_free(ec_key);
+    PKCS8_PRIV_KEY_INFO_free(p8inf);
+    OSSL_PROVIDER_unload(provider);
+    OPENSSL_free(ec_der);
+    return ok;
+}
 int setup_tests(void)
 {
     if (!test_get_libctx(&mainctx, &nullprov, NULL, NULL, NULL)) {
@@ -3558,6 +3650,7 @@ int setup_tests(void)
     ADD_TEST(test_new_keytype);
 #ifndef OPENSSL_NO_EC
     ADD_ALL_TESTS(test_d2i_PrivateKey_ex, 2);
+    ADD_TEST(test_d2i_PrivateKey_type_mismatch);
     ADD_TEST(test_ec_tofrom_data_select);
 #ifndef OPENSSL_NO_ECX
     ADD_TEST(test_ecx_tofrom_data_select);